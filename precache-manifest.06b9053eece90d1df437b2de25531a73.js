6f7b569df6558382db1a516586cc803cc5fcfeb4
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-06-05 11:43:20 -0400

    Updates

---
+++ precache-manifest.06b9053eece90d1df437b2de25531a73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1beade4ebed259da79e7360ef6fc14",
+    "url": "/restaurant-react-app/index.html"
+  },
+  {
+    "revision": "07267711549a4b900aa3",
+    "url": "/restaurant-react-app/static/css/main.75267ce8.chunk.css"
+  },
+  {
+    "revision": "fc6a0b0e116359af1695",
+    "url": "/restaurant-react-app/static/js/2.de246dc5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/restaurant-react-app/static/js/2.de246dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07267711549a4b900aa3",
+    "url": "/restaurant-react-app/static/js/main.8180632a.chunk.js"
+  },
+  {
+    "revision": "4cd6a73804261f7ed525",
+    "url": "/restaurant-react-app/static/js/runtime-main.5feaa0a8.js"
+  },
+  {
+    "revision": "3e791bbc63525a425117eaa6f19f1308",
+    "url": "/restaurant-react-app/static/media/restaurant.3e791bbc.jpg"
+  },
+  {
+    "revision": "5f9442912ed1fe842a0f13d58d4eb142",
+    "url": "/restaurant-react-app/static/media/tacos.5f944291.jpg"
+  }
+]);
